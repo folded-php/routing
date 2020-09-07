7b0115cb73f26335a69dbfd54afba9f04e26687f
Khalyomede <khalyomede@gmail.com>
2020-09-07 22:05:55 +0200

    added missing phpdoc for method matchRequestedMethod

--- src/matchRequestedUrl.php
+++ src/matchRequestedUrl.php
@@ -5,6 +5,17 @@ declare(strict_types = 1);
 namespace Folded;
 
 if (!function_exists("matchRequestedUrl")) {
+    /**
+     * Register route and try to match the current browsed URL.
+     *
+     * @since 0.1.0
+     *
+     * @throws Folded\Exceptions\UrlNotFoundException      If the URL is not found in the registered routes.
+     * @throws Folded\Exceptions\MethodNotAllowedException If the URL has been found, but the current protocol does not match.
+     *
+     * @example
+     * matchRequestedUrl();
+     */
     function matchRequestedUrl()
     {
         Router::startEngine();
